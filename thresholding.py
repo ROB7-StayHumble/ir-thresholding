bd39fe09cee687c3cfdd3cb15da2590349cd1a05
glhr <grenblot@gmail.com>
2019-12-02 15:15:23 +0100

    define masks for each image

--- thresholding.py
+++ thresholding.py
@@ -3,6 +3,16 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+masks = {
+         'ircam1571825132561437576.png':[200,385, 550,615],
+         'ircam1571825294694103257.png':[200,565, 500,642],
+         'ircam1571746715348190652.png':[195,405,600,670],
+         'ircam1571746625354059903.png':[170,245,415,445],
+         'ircam1571825078917436229.png':[195,400,385,460],
+         '1571825186737317588_IR.png':[180,265,580,625],
+         '1571825142073643588_IR.png':[200,480,0,93]
+         }
+
 def thresholding(imgpath):
     img = cv2.imread(imgpath)
     lo = np.mean(img) * 1.15
@@ -31,10 +41,13 @@ def thresholding(imgpath):
     return img
 
 def histo(imgpath):
+    print(imgpath)
     img = cv2.imread(imgpath)
     # create a mask
+
     mask = np.zeros(img.shape[:2], np.uint8)
-    mask[100:300, 100:400] = 255
+    y1,y2,x1,x2 = masks[imgpath][0],masks[imgpath][1],masks[imgpath][2],masks[imgpath][3]
+    mask[y1:y2, x1:x2] = 255
     masked_img = cv2.bitwise_and(img, img, mask=mask)
 
     # Calculate histogram with mask and without mask
@@ -42,15 +55,22 @@ def histo(imgpath):
     hist_full = cv2.calcHist([img], [0], None, [256], [0, 256])
     hist_mask = cv2.calcHist([img], [0], mask, [256], [0, 256])
 
+    hist_full_norm = cv2.normalize(hist_full, hist_full, 1, 0, cv2.NORM_L1)
+
+    hist_mask_norm = cv2.normalize(hist_mask, hist_mask, 1, 0, cv2.NORM_L1)
+
     plt.subplot(221), plt.imshow(img, 'gray')
     plt.subplot(222), plt.imshow(mask, 'gray')
     plt.subplot(223), plt.imshow(masked_img, 'gray')
-    plt.subplot(224), plt.plot(hist_full), plt.plot(hist_mask)
+    plt.subplot(224), plt.plot(hist_full_norm), plt.plot(hist_mask_norm)
+    plt.axvline(img.mean(), color='k', linestyle='dashed', linewidth=1)
     plt.xlim([0, 256])
+    min_ylim, max_ylim = plt.ylim()
+    plt.text(img.mean() * 1.1, max_ylim * 0.9, 'Mean: {:.2f}'.format(img.mean()))
 
     plt.show()
     return img
 
 for imgpath in glob.glob("*.png"):
-    thresholding(imgpath)
+    # thresholding(imgpath)
     histo(imgpath)
